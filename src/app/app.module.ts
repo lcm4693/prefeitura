1d570f283ced6b1ac028c61d9195724321f609cf
Diego Serpa <diego.neves@serpro.gov.br>
2022-06-20 20:31:26 -0300

    criação dos elementos table, alert e login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
-import { FormsModule } from '@angular/forms';
+import { AlertService } from './services/alert.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ConsultaImovelComponent } from './consulta-imovel/consulta-imovel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table'
+import { MatInputModule } from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ConsultaImovelComponent,
+    AlertComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [AlertService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,29 @@
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Component, Input, OnInit, Output } from '@angular/core';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { AlertService } from '../services/alert.service';
+import { Subscription } from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-login',
@@ -7,16 +31,42 @@ import { Component, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private subscription: Subscription = new Subscription();
+  message: any;
 
-  entrada: string = '';
-  senha: string = '';
+  login = new FormControl('', [Validators.required]);
+  senha = new FormControl('', [Validators.required]);
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private alertService: AlertService) {
 
-  ngOnInit(): void {
+    // this.formGroupTeste = this.formBuilder.group({
+    //   // *********************************************
+    //   // O valor padrão deste formControl será Marcelo
+    //   // e os demais vazio
+    //   // *********************************************
+    //   login: ['', Validators.required],
+    //   senha: ['', Validators.required],
+  // });
+
+   }
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  ngOnInit() {
+    this.subscription = this.alertService.getMessage().subscribe((message) => {
+      this.message = message;
+    });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
   logar(){
-    this.router.navigate(['/consulta'])
+    if(this.login.valid && this.senha.valid){
+      this.router.navigate(['/consulta'])
+    }else{
+      this.alertService.error("Existem campos não preenchidos no formulário");
+    }
   }
 }

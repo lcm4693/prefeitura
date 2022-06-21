036fd88fb090a9129e582b1758e1491042813389
Diego Serpa <diego.neves@serpro.gov.br>
2022-06-20 22:39:16 -0300

    Alteração alert

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,11 @@
-import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { ErrorStateMatcher } from '@angular/material/core';
@@ -13,60 +20,39 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  private subscription: Subscription = new Subscription();
-  message: any;
-
   login = new FormControl('', [Validators.required]);
   senha = new FormControl('', [Validators.required]);
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private alertService: AlertService) {
-
-    // this.formGroupTeste = this.formBuilder.group({
-    //   // *********************************************
-    //   // O valor padrão deste formControl será Marcelo
-    //   // e os demais vazio
-    //   // *********************************************
-    //   login: ['', Validators.required],
-    //   senha: ['', Validators.required],
-  // });
-
-   }
+  constructor(private router: Router, private alertService: AlertService) {}
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
 
-  ngOnInit() {
-    this.subscription = this.alertService.getMessage().subscribe((message) => {
-      this.message = message;
-    });
-  }
+  ngOnInit() {}
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-  logar(){
-    if(this.login.valid && this.senha.valid){
-      this.router.navigate(['/consulta'])
-    }else{
-      this.alertService.error("Existem campos não preenchidos no formulário");
+  logar() {
+    if (this.login.valid && this.senha.valid) {
+      this.router.navigate(['/consulta']);
+    } else {
+      this.alertService.error('Existem campos não preenchidos no formulário');
     }
   }
 }
